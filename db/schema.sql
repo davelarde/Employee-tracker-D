5d7b6dc201cc155f965e25b908463b8c3c68e3e5
danivelarde <danielavelarde4@gmail.com>
2022-05-02 18:18:03 -0400

    changes in schema

--- db/schema.sql
+++ db/schema.sql
@@ -14,8 +14,7 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
+   
 
 );
 
